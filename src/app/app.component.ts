7f18a8b1391b0934860da09884d2c31da7a28d42
Reme Ajayi <remeajayi@gmail.com>
2021-01-17 05:57:08 -0500

    final touches

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,62 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, OnInit } from '@angular/core';
+import { Video } from './video';
+import { interval, Subscription } from 'rxjs';
+import { takeWhile } from 'rxjs/operators';
+
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit, AfterViewInit{
+  // private subscription: Subscription;
+
   title = 'mall-monitor';
+  data = Video.data;
+  density = 0;
+  capacity = "0%";
+  max_density = 0;
+  max_capacity = "0%";
+  matched_people = 0;
+  detected_accessories: any;
+  mean_confidence = "0%";
+  max_confidence="0%";
+  count = 0;
+
+  constructor() { }
+
+  ngOnInit(): void {
+    this.count = this.data.length;
+
+  }
+
+  ngAfterViewInit(){  
+   this.changeVals();
+  }
+
+  increase(){
+    this.count++;
+  }
+
+
+
+  changeVals(){
+    let i = 0;
+    let interval = setInterval(()=>{
+      this.density = this.data[i].Current_Density;
+      this.capacity = this.data[i].Current_Capacity;
+      this.detected_accessories = 0;
+      this.matched_people = this.data[i].Matched_People;
+      this.max_capacity = this.data[i].Allowed_Max_Capacity;
+      this.max_confidence = this.data[i].Maximum_Confidence;
+      this.mean_confidence = this.data[i].Mean_Confidence;
+      i = i + 1;
+      if(i == this.count){
+        clearInterval(interval);
+      }
+    },5000);
+  }
+
 }
---
+++ src/app/video.ts
@@ -0,0 +1,494 @@
+export class Video  {
+    public static data = [
+        {
+            "Current_Density": 10,
+            "Current_Capacity": "33%",
+            "Allowed_Max_Density": 20,
+            "Allowed_Max_Capacity": "50%",
+            "Matched_People": 1,
+            "Detected_Accessories": null,
+            "Mean_Confidence": "77%",
+            "Maximum_Confidence": "99%"
+        },
+         {
+            "Current_Density": 11,
+            "Current_Capacity": "37%",
+            "Allowed_Max_Density": 20,
+            "Allowed_Max_Capacity": "55%",
+            "Matched_People": 1,
+            "Detected_Accessories": null,
+            "Mean_Confidence": "71%",
+            "Maximum_Confidence": "94%"
+        },
+         {
+            "Current_Density": 11,
+            "Current_Capacity": "37%",
+            "Allowed_Max_Density": 20,
+            "Allowed_Max_Capacity": "55%",
+            "Matched_People": 1,
+            "Detected_Accessories": null,
+            "Mean_Confidence": "76%",
+            "Maximum_Confidence": "97%"
+        },
+         {
+            "Current_Density": 12,
+            "Current_Capacity": "40%",
+            "Allowed_Max_Density": 20,
+            "Allowed_Max_Capacity": "60%",
+            "Matched_People": 1,
+            "Detected_Accessories": null,
+            "Mean_Confidence": "69%",
+            "Maximum_Confidence": "99%"
+        },
+         {
+            "Current_Density": 12,
+            "Current_Capacity": "40%",
+            "Allowed_Max_Density": 20,
+            "Allowed_Max_Capacity": "60%",
+            "Matched_People": 1,
+            "Detected_Accessories": null,
+            "Mean_Confidence": "77%",
+            "Maximum_Confidence": "98%"
+        },
+         {
+            "Current_Density": 12,
+            "Current_Capacity": "40%",
+            "Allowed_Max_Density": 20,
+            "Allowed_Max_Capacity": "60%",
+            "Matched_People": 1,
+            "Detected_Accessories": null,
+            "Mean_Confidence": "80%",
+            "Maximum_Confidence": "100%"
+        },
+         {
+            "Current_Density": 12,
+            "Current_Capacity": "40%",
+            "Allowed_Max_Density": 20,
+            "Allowed_Max_Capacity": "60%",
+            "Matched_People": 1,
+            "Detected_Accessories": null,
+            "Mean_Confidence": "78%",
+            "Maximum_Confidence": "100%"
+        },
+         {
+            "Current_Density": 12,
+            "Current_Capacity": "40%",
+            "Allowed_Max_Density": 20,
+            "Allowed_Max_Capacity": "60%",
+            "Matched_People": 1,
+            "Detected_Accessories": null,
+            "Mean_Confidence": "78%",
+            "Maximum_Confidence": "98%"
+        },
+        {
+            "Current_Density": 10,
+            "Current_Capacity": "33%",
+            "Allowed_Max_Density": 20,
+            "Allowed_Max_Capacity": "50%",
+            "Matched_People": 1,
+            "Detected_Accessories": null,
+            "Mean_Confidence": "74%",
+            "Maximum_Confidence": "97%"
+        },
+         {
+            "Current_Density": 9,
+            "Current_Capacity": "30%",
+            "Allowed_Max_Density": 20,
+            "Allowed_Max_Capacity": "45%",
+            "Matched_People": 1,
+            "Detected_Accessories": null,
+            "Mean_Confidence": "76%",
+            "Maximum_Confidence": "100%"
+        },
+         {
+            "Current_Density": 9,
+            "Current_Capacity": "30%",
+            "Allowed_Max_Density": 20,
+            "Allowed_Max_Capacity": "45%",
+            "Matched_People": 1,
+            "Detected_Accessories": null,
+            "Mean_Confidence": "78%",
+            "Maximum_Confidence": "99%"
+        },
+         {
+            "Current_Density": 9,
+            "Current_Capacity": "30%",
+            "Allowed_Max_Density": 20,
+            "Allowed_Max_Capacity": "45%",
+            "Matched_People": 1,
+            "Detected_Accessories": null,
+            "Mean_Confidence": "72%",
+            "Maximum_Confidence": "97%"
+        },
+        {
+            "Current_Density": 9,
+            "Current_Capacity": "30%",
+            "Allowed_Max_Density": 20,
+            "Allowed_Max_Capacity": "45%",
+            "Matched_People": 1,
+            "Detected_Accessories": null,
+            "Mean_Confidence": "71%",
+            "Maximum_Confidence": "98%"
+        },
+         {
+            "Current_Density": 9,
+            "Current_Capacity": "30%",
+            "Allowed_Max_Density": 20,
+            "Allowed_Max_Capacity": "45%",
+            "Matched_People": 1,
+            "Detected_Accessories": null,
+            "Mean_Confidence": "83%",
+            "Maximum_Confidence": "100%"
+        },
+        {
+            "Current_Density": 9,
+            "Current_Capacity": "30%",
+            "Allowed_Max_Density": 20,
+            "Allowed_Max_Capacity": "45%",
+            "Matched_People": 1,
+            "Detected_Accessories": null,
+            "Mean_Confidence": "74%",
+            "Maximum_Confidence": "100%"
+        },
+         {
+            "Current_Density": 9,
+            "Current_Capacity": "30%",
+            "Allowed_Max_Density": 20,
+            "Allowed_Max_Capacity": "45%",
+            "Matched_People": 1,
+            "Detected_Accessories": null,
+            "Mean_Confidence": "75%",
+            "Maximum_Confidence": "96%"
+        },
+        {
+            "Current_Density": 9,
+            "Current_Capacity": "30%",
+            "Allowed_Max_Density": 20,
+            "Allowed_Max_Capacity": "45%",
+            "Matched_People": 1,
+            "Detected_Accessories": null,
+            "Mean_Confidence": "73%",
+            "Maximum_Confidence": "92%"
+        },
+         {
+            "Current_Density": 9,
+            "Current_Capacity": "30%",
+            "Allowed_Max_Density": 20,
+            "Allowed_Max_Capacity": "45%",
+            "Matched_People": 1,
+            "Detected_Accessories": null,
+            "Mean_Confidence": "78%",
+            "Maximum_Confidence": "97%"
+        },
+         {
+            "Current_Density": 10,
+            "Current_Capacity": "33%",
+            "Allowed_Max_Density": 20,
+            "Allowed_Max_Capacity": "50%",
+            "Matched_People": 1,
+            "Detected_Accessories": null,
+            "Mean_Confidence": "72%",
+            "Maximum_Confidence": "99%"
+        },
+         {
+            "Current_Density": 10,
+            "Current_Capacity": "33%",
+            "Allowed_Max_Density": 20,
+            "Allowed_Max_Capacity": "50%",
+            "Matched_People": 1,
+            "Detected_Accessories": null,
+            "Mean_Confidence": "75%",
+            "Maximum_Confidence": "89%"
+        },
+         {
+            "Current_Density": 9,
+            "Current_Capacity": "30%",
+            "Allowed_Max_Density": 20,
+            "Allowed_Max_Capacity": "45%",
+            "Matched_People": 1,
+            "Detected_Accessories": null,
+            "Mean_Confidence": "72%",
+            "Maximum_Confidence": "93%"
+        },
+         {
+            "Current_Density": 10,
+            "Current_Capacity": "33%",
+            "Allowed_Max_Density": 20,
+            "Allowed_Max_Capacity": "50%",
+            "Matched_People": 1,
+            "Detected_Accessories": null,
+            "Mean_Confidence": "66%",
+            "Maximum_Confidence": "97%"
+        },
+        {
+            "Current_Density": 10,
+            "Current_Capacity": "33%",
+            "Allowed_Max_Density": 20,
+            "Allowed_Max_Capacity": "50%",
+            "Matched_People": 1,
+            "Detected_Accessories": null,
+            "Mean_Confidence": "72%",
+            "Maximum_Confidence": "95%"
+        },
+         {
+            "Current_Density": 10,
+            "Current_Capacity": "33%",
+            "Allowed_Max_Density": 20,
+            "Allowed_Max_Capacity": "50%",
+            "Matched_People": 1,
+            "Detected_Accessories": null,
+            "Mean_Confidence": "75%",
+            "Maximum_Confidence": "98%"
+        },
+        {
+            "Current_Density": 10,
+            "Current_Capacity": "33%",
+            "Allowed_Max_Density": 20,
+            "Allowed_Max_Capacity": "50%",
+            "Matched_People": 1,
+            "Detected_Accessories": null,
+            "Mean_Confidence": "73%",
+            "Maximum_Confidence": "99%"
+        },
+        {
+            "Current_Density": 10,
+            "Current_Capacity": "33%",
+            "Allowed_Max_Density": 20,
+            "Allowed_Max_Capacity": "50%",
+            "Matched_People": 1,
+            "Detected_Accessories": null,
+            "Mean_Confidence": "71%",
+            "Maximum_Confidence": "92%"
+        },
+        {
+            "Current_Density": 9,
+            "Current_Capacity": "30%",
+            "Allowed_Max_Density": 20,
+            "Allowed_Max_Capacity": "45%",
+            "Matched_People": 1,
+            "Detected_Accessories": null,
+            "Mean_Confidence": "79%",
+            "Maximum_Confidence": "100%"
+        },
+         {
+            "Current_Density": 9,
+            "Current_Capacity": "30%",
+            "Allowed_Max_Density": 20,
+            "Allowed_Max_Capacity": "45%",
+            "Matched_People": 1,
+            "Detected_Accessories": null,
+            "Mean_Confidence": "83%",
+            "Maximum_Confidence": "99%"
+        },
+         {
+            "Current_Density": 9,
+            "Current_Capacity": "30%",
+            "Allowed_Max_Density": 20,
+            "Allowed_Max_Capacity": "45%",
+            "Matched_People": 1,
+            "Detected_Accessories": null,
+            "Mean_Confidence": "77%",
+            "Maximum_Confidence": "98%"
+        },
+        {
+            "Current_Density": 10,
+            "Current_Capacity": "33%",
+            "Allowed_Max_Density": 20,
+            "Allowed_Max_Capacity": "50%",
+            "Matched_People": 1,
+            "Detected_Accessories": null,
+            "Mean_Confidence": "74%",
+            "Maximum_Confidence": "99%"
+        },
+        {
+            "Current_Density": 10,
+            "Current_Capacity": "33%",
+            "Allowed_Max_Density": 20,
+            "Allowed_Max_Capacity": "50%",
+            "Matched_People": 1,
+            "Detected_Accessories": null,
+            "Mean_Confidence": "80%",
+            "Maximum_Confidence": "99%"
+        },
+        {
+            "Current_Density": 10,
+            "Current_Capacity": "33%",
+            "Allowed_Max_Density": 20,
+            "Allowed_Max_Capacity": "50%",
+            "Matched_People": 1,
+            "Detected_Accessories": null,
+            "Mean_Confidence": "82%",
+            "Maximum_Confidence": "99%"
+        },
+        {
+            "Current_Density": 10,
+            "Current_Capacity": "33%",
+            "Allowed_Max_Density": 20,
+            "Allowed_Max_Capacity": "50%",
+            "Matched_People": 1,
+            "Detected_Accessories": null,
+            "Mean_Confidence": "78%",
+            "Maximum_Confidence": "100%"
+        },
+        {
+            "Current_Density": 10,
+            "Current_Capacity": "33%",
+            "Allowed_Max_Density": 20,
+            "Allowed_Max_Capacity": "50%",
+            "Matched_People": 1,
+            "Detected_Accessories": null,
+            "Mean_Confidence": "82%",
+            "Maximum_Confidence": "100%"
+        },
+        {
+            "Current_Density": 10,
+            "Current_Capacity": "33%",
+            "Allowed_Max_Density": 20,
+            "Allowed_Max_Capacity": "50%",
+            "Matched_People": 1,
+            "Detected_Accessories": null,
+            "Mean_Confidence": "84%",
+            "Maximum_Confidence": "100%"
+        },
+        {
+            "Current_Density": 10,
+            "Current_Capacity": "33%",
+            "Allowed_Max_Density": 20,
+            "Allowed_Max_Capacity": "50%",
+            "Matched_People": 1,
+            "Detected_Accessories": null,
+            "Mean_Confidence": "82%",
+            "Maximum_Confidence": "100%"
+        },
+        {
+            "Current_Density": 10,
+            "Current_Capacity": "33%",
+            "Allowed_Max_Density": 20,
+            "Allowed_Max_Capacity": "50%",
+            "Matched_People": 1,
+            "Detected_Accessories": null,
+            "Mean_Confidence": "81%",
+            "Maximum_Confidence": "100%"
+        },
+        {
+            "Current_Density": 10,
+            "Current_Capacity": "33%",
+            "Allowed_Max_Density": 20,
+            "Allowed_Max_Capacity": "50%",
+            "Matched_People": 1,
+            "Detected_Accessories": null,
+            "Mean_Confidence": "77%",
+            "Maximum_Confidence": "99%"
+        },
+        {
+            "Current_Density": 10,
+            "Current_Capacity": "33%",
+            "Allowed_Max_Density": 20,
+            "Allowed_Max_Capacity": "50%",
+            "Matched_People": 1,
+            "Detected_Accessories": null,
+            "Mean_Confidence": "87%",
+            "Maximum_Confidence": "99%"
+        },
+        {
+            "Current_Density": 10,
+            "Current_Capacity": "33%",
+            "Allowed_Max_Density": 20,
+            "Allowed_Max_Capacity": "50%",
+            "Matched_People": 1,
+            "Detected_Accessories": null,
+            "Mean_Confidence": "81%",
+            "Maximum_Confidence": "98%"
+        },
+        {
+            "Current_Density": 10,
+            "Current_Capacity": "33%",
+            "Allowed_Max_Density": 20,
+            "Allowed_Max_Capacity": "50%",
+            "Matched_People": 1,
+            "Detected_Accessories": null,
+            "Mean_Confidence": "82%",
+            "Maximum_Confidence": "100%"
+        },
+        {
+            "Current_Density": 10,
+            "Current_Capacity": "33%",
+            "Allowed_Max_Density": 20,
+            "Allowed_Max_Capacity": "50%",
+            "Matched_People": 1,
+            "Detected_Accessories": null,
+            "Mean_Confidence": "83%",
+            "Maximum_Confidence": "97%"
+        },
+        {
+            "Current_Density": 10,
+            "Current_Capacity": "33%",
+            "Allowed_Max_Density": 20,
+            "Allowed_Max_Capacity": "50%",
+            "Matched_People": 1,
+            "Detected_Accessories": null,
+            "Mean_Confidence": "80%",
+            "Maximum_Confidence": "99%"
+        },
+        {
+            "Current_Density": 10,
+            "Current_Capacity": "33%",
+            "Allowed_Max_Density": 20,
+            "Allowed_Max_Capacity": "50%",
+            "Matched_People": 1,
+            "Detected_Accessories": null,
+            "Mean_Confidence": "71%",
+            "Maximum_Confidence": "99%"
+        },
+        {
+            "Current_Density": 10,
+            "Current_Capacity": "33%",
+            "Allowed_Max_Density": 20,
+            "Allowed_Max_Capacity": "50%",
+            "Matched_People": 1,
+            "Detected_Accessories": null,
+            "Mean_Confidence": "82%",
+            "Maximum_Confidence": "98%"
+        },
+        {
+            "Current_Density": 10,
+            "Current_Capacity": "33%",
+            "Allowed_Max_Density": 20,
+            "Allowed_Max_Capacity": "50%",
+            "Matched_People": 1,
+            "Detected_Accessories": null,
+            "Mean_Confidence": "79%",
+            "Maximum_Confidence": "99%"
+        },
+         {
+            "Current_Density": 11,
+            "Current_Capacity": "37%",
+            "Allowed_Max_Density": 20,
+            "Allowed_Max_Capacity": "55%",
+            "Matched_People": 1,
+            "Detected_Accessories": null,
+            "Mean_Confidence": "77%",
+            "Maximum_Confidence": "99%"
+        },
+        {
+            "Current_Density": 11,
+            "Current_Capacity": "37%",
+            "Allowed_Max_Density": 20,
+            "Allowed_Max_Capacity": "55%",
+            "Matched_People": 1,
+            "Detected_Accessories": null,
+            "Mean_Confidence": "80%",
+            "Maximum_Confidence": "100%"
+        },
+        {
+            "Current_Density": 11,
+            "Current_Capacity": "37%",
+            "Allowed_Max_Density": 20,
+            "Allowed_Max_Capacity": "55%",
+            "Matched_People": 1,
+            "Detected_Accessories": null,
+            "Mean_Confidence": "77%",
+            "Maximum_Confidence": "99%"
+        }
+    ]
+}
